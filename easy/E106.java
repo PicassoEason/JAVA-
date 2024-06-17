16ca703d381d8352a246cd2c152830a8817e3c17
picassoeason <easonwu.student@outlook.com>
2024-06-17 20:59:50 +0800

    max

--- easy/E106.java
+++ easy/E106.java
@@ -1,6 +1,16 @@
 public class E106 {
     public static void main(String[] args) {
-        
+        int[] list={5,9,7};
+        System.out.println(maxinum(list));
+    }
+    public static Integer maxinum(int[] list){
+        int max=list[0];
+        for(int i:list){
+            if(i>max){
+                max=i;
+            }
+        }
+        return max;
     }
     
 }
