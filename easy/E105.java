6f9bf1d3fd687598046f63d1c3650f1565c4cedd
picassoeason <easonwu.student@outlook.com>
2024-06-17 19:35:58 +0800

    reverse

--- easy/E105.java
+++ easy/E105.java
@@ -1,3 +1,13 @@
 public class E105 {
-    
+    public static void main(String[] args) {
+        String word="6879";
+        System.out.println(reverse(word));
+    }
+    public static String reverse(String word){
+        String result="";
+        for(int i=0;i<word.length();i++){
+            result+=word.charAt(word.length()-i-1);
+        }
+        return result;
+    }
 }
