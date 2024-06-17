8a89031b320f1344e85c13c4ab5209747c424268
picassoeason <easonwu.student@outlook.com>
2024-06-17 21:07:22 +0800

    average -> arraylist

--- easy/E107.java
+++ easy/E107.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 public class E107 {
-    
+    public static void main(String[] args) {
+        List<Integer> l1 = new ArrayList<Integer>(); 
+        l1.add(0, 1);
+        l1.add(1, 9);  
+        System.out.println(average(l1));
+    }
+    public static double average(List <Integer> list){
+        return list.stream().mapToInt(i->i).average().getAsDouble();
+    }
 }
