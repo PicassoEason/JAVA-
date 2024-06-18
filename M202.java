c99f3c1af3648133c0129747c603533ab52936ef
picassoeason <easonwu.student@outlook.com>
2024-06-18 18:12:23 +0800

    isprime

--- M202.java
+++ M202.java
@@ -2,5 +2,8 @@ public class M202 {
     public static void main(String[] args) {
         
     }
-    
+    public static  Boolean isPrime(Integer n){
+        boolean isPrime=n>1&&java.math.BigInteger.valueOf(n).isProbablePrime(50);
+        return isPrime;
+    }
 }
