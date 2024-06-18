165aa39ff03dfb40793dce968aa393a9a74edba8
picassoeason <easonwu.student@outlook.com>
2024-06-18 18:20:53 +0800

    m103 need to help

---
+++ M103.java
@@ -0,0 +1,11 @@
+// public class M103 {
+//     public static void main(String[] args) {
+//         System.out.println(fibonacci(8));   
+//     }
+//     public static Integer fibonacci(Integer n) {
+//         if (n == null) {
+//             return null;
+//         }
+//         return (n <= 1) ? n : fibonacci(n - 1) + fibonacci(n - 2);
+//     }
+// }
