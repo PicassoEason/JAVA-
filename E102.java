4816189d028c6ec9965daeaa929ef07c58459c3f
UNKNOWNREM <jamieyang920108@gmail.com>
2024-06-19 09:43:41 +0800

    Factorial

--- E102.java
+++ E102.java
@@ -1,10 +1,8 @@
 
-import java.util.Scanner;
-
 public class E102 {
     public static void main(String[] args) {
-        Scanner sc=new  Scanner(System.in);
-        int n=sc.nextInt();
+        int n = 3;
+        sum(n);
         System.out.println(sum(n));
     }
     public static int sum(Integer n){
