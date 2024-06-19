d3399ad3f65860ecd115da2421fabfd4105b46fb
UNKNOWNREM <jamieyang920108@gmail.com>
2024-06-19 09:40:24 +0800

    sum

--- E101.java
+++ E101.java
@@ -1,12 +1,9 @@
 
-import java.util.Scanner;
 
 public class E101{
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        System.out.println(sum(a,b));
+        sum(3, 4);
+        sum(5, 6);
     }
     public static int sum(Integer a,Integer b){
         return a+b;
